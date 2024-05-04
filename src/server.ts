21c81df1667c635a5f7d3e3a86faaf659621e91a
Miguel Souza <tp.miguel.souza@totvspartners.com.br>
2024-05-04 13:16:30 -0300

    improve code

--- src/server.ts
+++ src/server.ts
@@ -4,16 +4,14 @@ import { MessageChannelController } from './controllers'
 import { CustomerService } from './services/customer-service'
 import { MessageChannel } from './usecases'
 require('dotenv').config()
-import express from 'express';
+import express from 'express'
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
 const server = require('http').createServer(app)
-
 app.use(bodyParser.json({ limit: '50mb', type: 'application/json' }))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
-
 
 app.use(cors())
 
@@ -31,9 +29,24 @@ app.use(express.json())
 
 const conversations = new NodeCache()
 
-app.post('/api/receive-message', async (req: express.Request, res: express.Response) => {
-  messageChannelController.waitingMessage(req, res, conversations)
+// Tratamento de exceções não capturadas
+process.on('uncaughtException', function (err) {
+  console.error('Exceção não capturada:', err)
+  // Você pode optar por reiniciar a aplicação ou tomar outras medidas adequadas aqui
 })
+
+// Tratamento de promessas não tratadas (rejeitadas)
+process.on('unhandledRejection', function (err, promise) {
+  console.error('Rejeição não tratada:', err)
+  // Você pode optar por reiniciar a aplicação ou tomar outras medidas adequadas aqui
+})
+
+app.post(
+  '/api/receive-message',
+  async (req: express.Request, res: express.Response) => {
+    messageChannelController.waitingMessage(req, res, conversations)
+  },
+)
 
 app.get('/api/test', async (req: express.Request, res: express.Response) => {
   res.send('Server no ar')
