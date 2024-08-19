2f314fffe4e921f580bd40c9b71172371a12bdb1
Miguel Souza <tp.miguel.souza@totvspartners.com.br>
2024-08-19 10:40:29 -0300

    add console

--- src/usecases/message-channel.usecase.ts
+++ src/usecases/message-channel.usecase.ts
@@ -170,7 +170,7 @@ export class MessageChannel {
             )
             console.log(linkVideo.UrlChat)
             const urlCurta = await TinyURL.shorten(linkVideo.UrlChat)
-
+            console.log('urlllllll', urlCurta)
             message.body = [
               'Nossa Teletriagem inteligente combina tecnologias de inteligência artificial e linguagem natural para te ajudar no início dos sintomas e orientar para o melhor desfecho de acordo com seu nível de urgência. O desfecho da sua Teletriagem pode ser o encaminhamento para uma Teleconsulta por vídeo, orientações de saúde ou necessidade de atendimento presencial. Para seguir com seu atendimento de Teletriagem inteligente, clique no link abaixo 👇\n' +
                 `${urlCurta}\n\n` +
